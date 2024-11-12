df67acf712be0db28a367d2edd88641e196574a1
AkmalIT <akmalrustamov0203@gmail.com>
2024-11-12 22:09:40 +0500

    ..

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-function addSum(a, b) {
-  return a + b;
-}
+var titleToNumber = function (columnTitle) {
+  let result = 0;
+  let power = 0;
 
-addSum(2, 3);
+  for (let i = columnTitle.length - 1; i >= 0; i--) {
+    const charValue = columnTitle.charCodeAt(i) - "A".charCodeAt(0) + 1;
+    result += charValue * Math.pow(26, power);
+    power++;
+  }
+
+  return result;
+};
+
+console.log(titleToNumber('AB'));
